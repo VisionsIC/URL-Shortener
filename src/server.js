d8b179bc314567c094056440c4af94a5c33c624a
Aaron Wilson <visionsic@fullsail.edu>
2017-06-01 19:11:25 -0400

    Basic App Functionality

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 // Application Requirements and variables.
 var express = require('express');
-var bodyParser = require('body-parser');
+//node var bodyParser = require('body-parser');
 var app = express();
-
 // Configuration Variables.
 var port = 3000;
-app.get('/', function (req, res) {
-  res.send('Server is successfully active on port ', port)
+// JSON Request Test.
+app.get('/talk', function (req,res) {
+  res.json({Hello: 'World!'});
 });
-app.listen(port);
+// Application Listening to the port at 3000.
+app.listen(port, function() {
+  console.log('Server is successfully active on port', port);
+});
