9178f282a4d6494461fe50e2b3a9a64ecb25c5b1
Aaron Wilson <visionsic@fullsail.edu>
2017-06-01 22:12:08 -0400

    Custom Mocha Testing

--- test/__api.js
+++ test/__api.js
@@ -1,8 +1,7 @@
-var assert = require('assert');
-describe('Balls', function() {
-  describe('ContainerOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+var expect = require('expect');
+// Mocha Test One
+describe('API', function() {
+  it('should probably load', function() {
+    expect(100).toBe(100);
   });
 });
