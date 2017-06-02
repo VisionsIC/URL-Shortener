6ca85e71e9704d91363da87c2e56ea8cd09867bd
Aaron Wilson <visionsic@fullsail.edu>
2017-06-02 14:39:29 -0400

    Added Three SuperTest Unit tests for the API

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,15 @@ module.exports = function (express) {
   var router = express.Router();
   // JSON Request Test.
   router.get('/request', function (req,res) {
-    res.json({Hello: 'World!'});
+    res.json({hello: 'world'});
   });
   // JSON Status Test.
   router.get('/status', function (req,res) {
-    res.json({Healthy: 'True'});
+    res.json({healthy: 'true'});
+  });
+  // JSON User ID Test.
+  router.get('/user/:id', function (req,res) {
+    res.json({user: {id: req.params.id}});
   });
   return router;
 };
--- test/__api.js
+++ test/__api.js
@@ -1,19 +1,39 @@
-var expect = require('expect');
 // Add the Supertest Module.
 var request = require('supertest');
 // Mocha Test One
 describe('API', function(){
   var server;
   // Start Server for testing.
-  beforeEach( function(){
-    var server = require('../src/server.js');
+  beforeEach(function(){
+    server = require('../src/server.js');
   });
   // Close Server when testing is done.
-  afterEach( function(){
+  afterEach(function(){
     server.close();
   });
-  // Commense the test.
-  it('should probably load', function() {
-    expect(100).toBe(100);
+  // Commense the Request test.
+  it('This should initiate the REQUEST test.', function testRequest(done){
+    request(server)
+    .get('/api/request')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200, {hello: 'world'}, done)
+  });
+  // Commense the Request test.
+  it('This should initiate the STATUS test.', function testHealth(done){
+    request(server)
+    .get('/api/status')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200, {healthy: 'true'}, done)
+  });
+  // Commense the Request test.
+  it('This should initiate the USER ID test.', function testUserID(done){
+    var fakeUserID = 347
+    request(server)
+    .get('/api/user/' + fakeUserID)
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200, {user: {id: fakeUserID }}, done)
   });
 });
