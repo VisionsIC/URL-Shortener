43553ecb9f9a1592b474db7f2b74c883204307ba
Aaron Wilson <visionsic@fullsail.edu>
2017-06-03 12:16:36 -0400

    Started building out JQuery functionality

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,8 +1,12 @@
 // JQuery to engage the SUBMIT button and POST the entered url to be shortened
 // by the API.
 
-$(#submit).click(function(){
+// Start by building out an anonymous function to house other necessary control functionality.
+$(function(){
+    // This is the function to control the click event of the "submit" button.
+    $('#submit').click(function(){
+        // Create a "normalUrl" variable to gain control of the "normal-url" input field.
+       var normalUrl = $('#normal-url').val();
 
-
-  
+       });
 });
