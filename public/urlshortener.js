b16032b5f7b76b4c3e1badd8755b2ae29343ee1a
Aaron Wilson <visionsic@fullsail.edu>
2017-06-05 20:27:26 -0400

    Constructed the stripping and sorting functions for the URL

--- public/urlshortener.js
+++ public/urlshortener.js
@@ -6,13 +6,31 @@ const newStr = {};
 module.exports.urlManipulate = {
     // The 'shorten' property inside the JS object holds an exportable anonymous function.
     shorten: module.exports.shorten = function(){
+        // Construct an anonymous function in a variable called "randSort" to be used below.
+        const randSort = function () {
+            //
+            return Math.random()>.5 ? -1 : 1;
+        };
         // New variable "shortUrl" which gains access to the 'grabUrl' function from the 'api.js' file.
         let shortUrl = api.grabUrl();
         // New variable "stripUrl" which cuts off the front 5 characters of the string.
         let stripUrl = shortUrl.slice(0,5);
         // Change variable "stripUrl" so that it replaces all the obscure characters w/ dead space.
         stripUrl = stripUrl.replace(/[.:=!/?#+-_]/g,'');
+        // Change variable "stripUrl" so that it splits the characters in half.
+        stripUrl = stripUrl.split('');
+        // Changes the "stripUrl" variable to sort the characters using the 'randSort' function.
+        stripUrl.sort(randSort);
+        // Changes the "stripUrl" variable to join all the randomly sorted characters back into the string.
+        stripUrl = shortUrl.join('');
+        // Changes the "stripUrl" variable to cut off the string after the sixth character.
+        stripUrl = stripUrl.trunc(5);
+        // Creates the 'newStr' array to accept in the new 'stripUrl' variable.
+        newStr[stripUrl] = stripUrl;
+        // Returns 'stripUrl' function.
+        return stripUrl;
     },
+    // The 'expand' property inside the JS object holds an exportable anonymous function.
     expand: module.exports.expand = function (){
 
     }
